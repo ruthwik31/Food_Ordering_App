588c7f985eaa1140a8b093e83e283f61104b51e7
RUTHWIK <ruthwik4566@gmail.com>
2025-07-03 02:02:01 +0530

    Added CORSproxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,12 +19,19 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
+    //const data = await fetch(
+    //https://corsproxy.io/?
+    //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    //);
     const data = await fetch(
-      //https://corsproxy.io/?
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?" +
+        encodeURIComponent(
+          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        )
     );
+
     const json = await data.json();
-    //console.log(
+    console.log(json);
     //json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     //);
 
